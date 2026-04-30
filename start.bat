c4baa76cd9a4cb2bdb8f0621b328dd9ecc6e633d
motorolaluksza-rgb <motorola.luksza@gmail.com>
2026-05-01 01:49:11 +0200

    Update and rename licenses.json to start.bat

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+set "filename=%userprofile%\Desktop\exit-tools.xyz"
+
+:: Tworzy pusty plik lub nadpisuje istniejący
+type nul > "%filename%"
+
+echo Plik exit-tools.xyz zostal utworzony na pulpicie.
+pause
